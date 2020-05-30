7336a72c6bd524c4aa1ecd682b621fe737ec8e88
MrRisoni <vanguelisk@gmail.com>
2020-05-30 13:20:18 +0300

    db bkp

--- foradb_dev.sql
+++ foradb_dev.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 30, 2020 at 05:50 AM
--- Server version: 8.0.20
+-- Generation Time: May 30, 2020 at 10:19 AM
+-- Server version: 10.4.12-MariaDB
 -- PHP Version: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,13 +25,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `articles`
+--
+
+CREATE TABLE `articles` (
+  `art_id` bigint(20) NOT NULL,
+  `art_title` varchar(255) DEFAULT NULL,
+  `art_langid` int(11) NOT NULL,
+  `art_created_at` datetime DEFAULT NULL,
+  `art_updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
 CREATE TABLE `migrations` (
-  `id` bigint UNSIGNED NOT NULL,
-  `file_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `processed` tinyint UNSIGNED NOT NULL DEFAULT '0',
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `file_name` varchar(255) NOT NULL,
+  `processed` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
   `created_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -41,7 +56,12 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `file_name`, `processed`, `created_at`) VALUES
 (1, 'migration20200528_090408_users', 1, '2020-04-28 17:48:19'),
 (9, 'migration20200528_102626_tickets', 1, '2020-05-28 07:26:26'),
-(10, 'migration20200528_103652_AddIndexToTickets', 1, '2020-05-28 07:36:52');
+(10, 'migration20200528_103652_AddIndexToTickets', 1, '2020-05-28 07:36:52'),
+(12, 'migration20200530_094028_AddColumnsToUser', 1, NULL),
+(16, 'migration20200530_114123_RenameColumnInusers', 1, '2020-05-30 08:41:23'),
+(17, 'migration20200530_123520_RemoveColumnsFromusers', 1, '2020-05-30 09:35:20'),
+(19, 'migration20200530_125534_DropTablesfoobar', 1, '2020-05-30 09:55:34'),
+(20, 'migration20200530_010955_articles', 1, '2020-05-30 10:09:55');
 
 -- --------------------------------------------------------
 
@@ -50,8 +70,8 @@ INSERT INTO `migrations` (`id`, `file_name`, `processed`, `created_at`) VALUES
 --
 
 CREATE TABLE `tickets` (
-  `tkt_id` bigint NOT NULL,
-  `tkt_bookingId` int DEFAULT NULL,
+  `tkt_id` bigint(20) NOT NULL,
+  `tkt_bookingId` int(11) DEFAULT NULL,
   `tkt_pnr` varchar(255) DEFAULT NULL,
   `tkt_ticket_no` varchar(255) DEFAULT NULL,
   `tkt_created_at` datetime DEFAULT NULL,
@@ -65,15 +85,23 @@ CREATE TABLE `tickets` (
 --
 
 CREATE TABLE `users` (
-  `usr_id` int UNSIGNED NOT NULL,
+  `usr_id` int(10) UNSIGNED NOT NULL,
   `usr_title` varchar(255) NOT NULL,
   `usr_created_at` datetime NOT NULL,
-  `usr_updated_at` datetime NOT NULL
+  `usr_updated_at` datetime NOT NULL,
+  `usr_email` varchar(255) DEFAULT NULL,
+  `usr_hash_passwd` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`art_id`,`art_langid`);
 
 --
 -- Indexes for table `migrations`
@@ -99,22 +127,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `art_id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `tkt_id` bigint NOT NULL AUTO_INCREMENT;
+  MODIFY `tkt_id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `usr_id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `usr_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
