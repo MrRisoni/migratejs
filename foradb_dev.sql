855acbde8f439313c1b0c6d99f5367634dc8e9d6
MrRisoni <vanguelisk@gmail.com>
2020-06-02 14:39:54 +0300

    backup

--- foradb_dev.sql
+++ foradb_dev.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 30, 2020 at 10:19 AM
--- Server version: 10.4.12-MariaDB
+-- Generation Time: Jun 02, 2020 at 11:39 AM
+-- Server version: 8.0.20
 -- PHP Version: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,27 +24,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `articles`
---
-
-CREATE TABLE `articles` (
-  `art_id` bigint(20) NOT NULL,
-  `art_title` varchar(255) DEFAULT NULL,
-  `art_langid` int(11) NOT NULL,
-  `art_created_at` datetime DEFAULT NULL,
-  `art_updated_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `migrations`
 --
 
 CREATE TABLE `migrations` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `file_name` varchar(255) NOT NULL,
-  `processed` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
+  `id` bigint UNSIGNED NOT NULL,
+  `file_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `processed` tinyint UNSIGNED NOT NULL DEFAULT '0',
   `created_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -56,12 +41,48 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `file_name`, `processed`, `created_at`) VALUES
 (1, 'migration20200528_090408_users', 1, '2020-04-28 17:48:19'),
 (9, 'migration20200528_102626_tickets', 1, '2020-05-28 07:26:26'),
-(10, 'migration20200528_103652_AddIndexToTickets', 1, '2020-05-28 07:36:52'),
-(12, 'migration20200530_094028_AddColumnsToUser', 1, NULL),
-(16, 'migration20200530_114123_RenameColumnInusers', 1, '2020-05-30 08:41:23'),
-(17, 'migration20200530_123520_RemoveColumnsFromusers', 1, '2020-05-30 09:35:20'),
-(19, 'migration20200530_125534_DropTablesfoobar', 1, '2020-05-30 09:55:34'),
-(20, 'migration20200530_010955_articles', 1, '2020-05-30 10:09:55');
+(15, 'migration20200528_103652_AddIndexToTickets', 1, '2020-05-30 06:17:29'),
+(17, 'migration20200530_094028_AddColumnsToUser', 1, '2020-05-30 06:40:28'),
+(18, 'migration20200530_021633_Rank', 1, '2020-05-30 11:16:33'),
+(27, 'migration20200602_101038_AddForeignKeyTousersReferencesranks', 1, '2020-06-02 07:10:38'),
+(28, 'migration20200602_102145_Thread', 1, '2020-06-02 07:21:45'),
+(30, 'migration20200602_115716_AddColumnsTo_users', 1, '2020-05-28 17:48:19'),
+(33, 'migration20200602_124417_AddColumnsTo_threads', 1, '2020-06-02 09:44:17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ranks`
+--
+
+CREATE TABLE `ranks` (
+  `rnk_id` tinyint NOT NULL,
+  `rnk_title` varchar(255) DEFAULT NULL,
+  `rnk_created_at` datetime DEFAULT NULL,
+  `rnk_updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ranks`
+--
+
+INSERT INTO `ranks` (`rnk_id`, `rnk_title`, `rnk_created_at`, `rnk_updated_at`) VALUES
+(1, 'Adobe', '2020-06-02 10:16:27', '2020-06-02 10:16:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `threads`
+--
+
+CREATE TABLE `threads` (
+  `thrd_id` bigint NOT NULL,
+  `thrd_title` varchar(80) NOT NULL,
+  `thrd_upvotes` tinyint UNSIGNED DEFAULT '0',
+  `thrd_created_at` datetime DEFAULT NULL,
+  `thrd_updated_at` datetime DEFAULT NULL,
+  `thrd_downvotes` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -70,8 +91,8 @@ INSERT INTO `migrations` (`id`, `file_name`, `processed`, `created_at`) VALUES
 --
 
 CREATE TABLE `tickets` (
-  `tkt_id` bigint(20) NOT NULL,
-  `tkt_bookingId` int(11) DEFAULT NULL,
+  `tkt_id` bigint NOT NULL,
+  `tkt_bookingId` int DEFAULT NULL,
   `tkt_pnr` varchar(255) DEFAULT NULL,
   `tkt_ticket_no` varchar(255) DEFAULT NULL,
   `tkt_created_at` datetime DEFAULT NULL,
@@ -85,29 +106,45 @@ CREATE TABLE `tickets` (
 --
 
 CREATE TABLE `users` (
-  `usr_id` int(10) UNSIGNED NOT NULL,
+  `usr_id` int UNSIGNED NOT NULL,
   `usr_title` varchar(255) NOT NULL,
   `usr_created_at` datetime NOT NULL,
   `usr_updated_at` datetime NOT NULL,
   `usr_email` varchar(255) DEFAULT NULL,
-  `usr_hash_passwd` varchar(255) DEFAULT NULL
+  `usr_dob` date DEFAULT '2010-01-01',
+  `usr_passwd` varchar(255) DEFAULT NULL,
+  `usr_ranks_id` tinyint DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`usr_id`, `usr_title`, `usr_created_at`, `usr_updated_at`, `usr_email`, `usr_dob`, `usr_passwd`, `usr_ranks_id`) VALUES
+(1, 'dsdsd', '2020-06-02 12:02:54', '2020-06-02 12:02:54', NULL, '2010-01-01', 'sdsds', 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `articles`
---
-ALTER TABLE `articles`
-  ADD PRIMARY KEY (`art_id`,`art_langid`);
-
---
 -- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `file_name` (`file_name`);
+
+--
+-- Indexes for table `ranks`
+--
+ALTER TABLE `ranks`
+  ADD PRIMARY KEY (`rnk_id`);
+
+--
+-- Indexes for table `threads`
+--
+ALTER TABLE `threads`
+  ADD PRIMARY KEY (`thrd_id`);
 
 --
 -- Indexes for table `tickets`
@@ -120,35 +157,52 @@ ALTER TABLE `tickets`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`usr_id`);
+  ADD PRIMARY KEY (`usr_id`),
+  ADD KEY `usr_ranks_id` (`usr_ranks_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `articles`
---
-ALTER TABLE `articles`
-  MODIFY `art_id` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT for table `ranks`
+--
+ALTER TABLE `ranks`
+  MODIFY `rnk_id` tinyint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `threads`
+--
+ALTER TABLE `threads`
+  MODIFY `thrd_id` bigint NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `tkt_id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `tkt_id` bigint NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `usr_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `usr_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`usr_ranks_id`) REFERENCES `ranks` (`rnk_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
